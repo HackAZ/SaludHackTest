68da0a135ff5c6d44dd5de2405e149e544137c87
Lisa Zhang <amphithere.19@gmail.com>
2016-01-23 11:36:35 -0700

    CSS and distance between providers added

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -8,7 +8,9 @@ class ProvidersController < ApplicationController
   end
 
   def search
-    @services = Provider.where(provider_type: params['services']).where(specialization: params['specialization'])
+    # @location = current_user
+    # @services = Provider.where(provider_type: params['services']).where(specialization: params['specialization'])
+    @locations = Provider.near(current_user, 50, :order => "distance")
   end
 
   # GET /providers/1
