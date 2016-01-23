407f83450c67a239ad075c97f9af46f283e057d3
Lisa Zhang <amphithere.19@gmail.com>
2016-01-23 09:19:32 -0700

    Heroku is annoying

--- app/assets/javascripts/providers.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    L.mapbox.accessToken = "pk.eyJ1IjoiYW1waGl0aGVyZSIsImEiOiIyZGE2MWJhYTdlNjJkM2E4NTZhYjUyODE3NDFmNzY3MSJ9.EDkcexR3h8zGfuPOXADctQ";
-    var map = L.mapbox.map('map', 'amphithere.nanckefd');
-  }
